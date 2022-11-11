c820eb7e85fac69e31f75b50c41a5bf9b6a8154f
Danughh <109573892+Danughh@users.noreply.github.com>
2022-11-11 10:28:55 -0600

    Add files via upload

---
+++ Sánchez Gutiérrez_Examen_DAW/js/resisist.js
@@ -0,0 +1,188 @@
+var res1 = 0;
+var res2 = 0;
+var res3 = 0;
+var res4 = 0;
+
+function resistencia1() {
+    res1++;
+    result = document.getElementById("resistencia1");
+    switch (res1) {
+        case 1:
+          
+           result.style.background = "brown";
+            break;
+
+            case 2:
+            result.style.background = "red";
+            break;
+
+            case 3:
+                result.style.background = "orange";
+            break;
+
+            case 4:
+                result.style.background = "yellow";
+            break;
+
+            case 5:
+                result.style.background = "green";
+            break;
+            case 6:
+                result.style.background = "blue";
+            break;
+            case 7:
+                result.style.background = "violet";
+            break;
+            case 8:
+                result.style.background = "gray";
+            break;
+            case 9:
+                result.style.background = "white";
+                res1 = 0;
+            break;
+        default:
+            result.style.background = "black";
+            break;
+    }
+}
+
+function resistencia2() {
+    res2++;
+    result = document.getElementById("resistencia2");
+    switch (res2) {
+        case 1:
+          
+           result.style.background = "brown";
+            break;
+
+            case 2:
+            result.style.background = "red";
+            break;
+
+            case 3:
+                result.style.background = "orange";
+            break;
+
+            case 4:
+                result.style.background = "yellow";
+            break;
+
+            case 5:
+                result.style.background = "green";
+            break;
+            case 6:
+                result.style.background = "blue";
+            break;
+            case 7:
+                result.style.background = "violet";
+            break;
+            case 8:
+                result.style.background = "gray";
+            break;
+            case 9:
+                result.style.background = "white";
+                res2 = 0;
+            break;
+        default:
+            result.style.background = "black";
+            break;
+    }
+}
+
+function resistencia3() {
+    res3++;
+    result = document.getElementById("resistencia3");
+    switch (res3) {
+        case 1:
+          
+           result.style.background = "brown";
+            break;
+
+            case 2:
+            result.style.background = "red";
+            break;
+
+            case 3:
+                result.style.background = "orange";
+            break;
+
+            case 4:
+                result.style.background = "yellow";
+            break;
+
+            case 5:
+                result.style.background = "green";
+            break;
+            case 6:
+                result.style.background = "blue";
+            break;
+            case 7:
+                result.style.background = "violet";
+            break;
+            case 8:
+                result.style.background = "gray";
+            break;
+            case 9:
+                result.style.background = "white";
+                res3 = 0;
+            break;
+        default:
+            result.style.background = "black";
+            break;
+    }
+}
+
+function resistencia4() {
+    res4++;
+    result = document.getElementById("resistencia4");
+    switch (res4) {
+        case 1:
+          
+           result.style.background = "brown";
+            break;
+
+            case 2:
+            result.style.background = "red";
+            break;
+
+            case 3:
+                result.style.background = "orange";
+            break;
+
+            case 4:
+                result.style.background = "yellow";
+            break;
+
+            case 5:
+                result.style.background = "green";
+            break;
+            case 6:
+                result.style.background = "blue";
+            break;
+            case 7:
+                result.style.background = "violet";
+            break;
+            case 8:
+                result.style.background = "gray";
+            break;
+            case 9:
+                result.style.background = "white";
+              
+            break;
+            case 10:
+                result.style.background = "gold";
+
+            break;
+            case 11:
+                result.style.background = "silver";
+                res4 = 0;
+            break;
+        default:
+            result.style.background = "gold";
+            break;
+    }
+}
+
+function calculores(){
+
+}
